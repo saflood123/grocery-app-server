7c308c50f598097646952f7a9f59efabb40223dc
saflood123 <stuartflood@gmail.com>
2025-01-11 14:10:34 +0000

    added exercise table

---
+++ Sources/App/Migrations/CreateExerciseTableMigration.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  grocery-app-server
+//
+//  Created by stuart flood on 22/12/2024.
+//
+
+import Foundation
+import Fluent
+
+struct CreateExerciseTableMigration: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("exercise")
+            .id()
+            .field("exercise", .string, .required).unique(on: "exercise")
+            .field("category", .string, .required)
+            //.field("email", .string, .required)
+            .create()
+    }
+    //undo
+    func revert(on database: Database) async throws {
+        try await database.schema("exercise")
+            .delete()
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,20 +10,22 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
     
-    if let databaseURL = Environment.get("DATABASE_URL") {
-        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
-        tlsConfig.certificateVerification = .none
-        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
-
-        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
-        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
-
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-    } else {
-        app.databases.use(.postgres(hostname: Environment.get("DB_HOST_NAME") ?? "localhost",  username: Environment.get("DB_USER_NAME") ?? "postgres", password: Environment.get("DB_PASSWORD") ?? "", database: Environment.get("DB_NAME") ?? "grocerydb"), as: .psql)
-    }
+//    if let databaseURL = Environment.get("DATABASE_URL") {
+//        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+//        tlsConfig.certificateVerification = .none
+//        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
+//
+//        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+//        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+//
+//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+//    } else {
+//        app.databases.use(.postgres(hostname: Environment.get("DB_HOST_NAME") ?? "localhost",  username: Environment.get("DB_USER_NAME") ?? "postgres", password: Environment.get("DB_PASSWORD") ?? "", database: Environment.get("DB_NAME") ?? "grocerydb"), as: .psql)
+//    }
     
 //    app.databases.use(.postgres(hostname: Environment.get("DB_HOST_NAME") ?? "localhost",  username: Environment.get("DB_USER_NAME") ?? "postgres", password: Environment.get("DB_PASSWORD") ?? "", database: Environment.get("DB_NAME") ?? "grocerydb"), as: .psql)
+    
+    app.databases.use(.postgres(hostname: "localhost",  username:  "postgres", password:  "", database: "grocerydb"), as: .psql)
     
     //register migrations
     app.migrations.add(CreateUsersTableMigration())
@@ -34,7 +36,7 @@ public func configure(_ app: Application) async throws {
     try app.register(collection: UserController())
     try app.register(collection: GroceryController())
     
-    app.jwt.signers.use(.hs256(key: Environment.get("JWT_SIGN_KEY") ?? "SECRETKEY"))
+    //app.jwt.signers.use(.hs256(key: Environment.get("JWT_SIGN_KEY") ?? "SECRETKEY"))
     
     //register routes
     try routes(app)
