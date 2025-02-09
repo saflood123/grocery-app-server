5acce2756cf33f35359836494b7bc61833135be7
saflood123 <stuartflood@gmail.com>
2025-02-09 16:22:07 +0000

    date

--- Sources/App/Extensions/GroceryItemResponseDTO+Extensions.swift
+++ Sources/App/Extensions/GroceryItemResponseDTO+Extensions.swift
@@ -16,6 +16,7 @@ extension GroceryItemResponseDTO: Content {
         guard let groceryItemId = groceryItem.id else {
             return nil
             }
-        self.init(id: groceryItemId, title: groceryItem.title,price: groceryItem.price, quantity: groceryItem.quantity)
+        self.init(id: groceryItemId, title: groceryItem.title,price: groceryItem.price,
+                  quantity: groceryItem.quantity)
     }
 }
