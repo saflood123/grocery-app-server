cebe70b1075859a101c92352847a35634bd8a731
saflood123 <stuartflood@gmail.com>
2025-04-19 12:51:25 +0100

    grocery 3 migration

---
+++ Sources/App/Migrations/CreateGroceryItem3TableMigration.swift
@@ -0,0 +1,29 @@
+//
+//  CreateGroceryItemTableMigration.swift
+//  grocery-app-server
+//
+//  Created by stuart flood on 29/12/2024.
+//
+import Foundation
+import Fluent
+
+class CreateGroceryItem3TableMigration: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("grocery_items3")
+            .id()
+            .field("title", .string,  .required)
+            .field("price", .double, .required)
+            .field("quantity", .int, .required)
+            .field("calories", .int, .required)
+            .field("steps", .int, .required)
+            .field("dateofbirth", .string)
+            .field("date_updated", .string)
+            .field("grocery_category_id", .uuid, .required, .references("grocery_categories","id", onDelete: .cascade))
+           // .field("grocery_category_id", .uuid, .required, .references("grocery_categories","id", onDelete: .cascade))
+            .create()
+    }
+    func revert(on database: any Database) async throws {
+      try await database.schema("grocery_items3")
+            .delete()
+    }
+}
